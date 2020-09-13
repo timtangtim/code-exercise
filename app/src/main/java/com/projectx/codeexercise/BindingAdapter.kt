6391b774500d24a5db4c6206e4056b6407b6f4cf
tangtszwai <tszwaitang@gmail.com>
2020-09-13 13:01:29 +0800

    Add more detail for weather

--- app/src/main/java/com/projectx/codeexercise/BindingAdapter.kt
+++ app/src/main/java/com/projectx/codeexercise/BindingAdapter.kt
@@ -7,11 +7,31 @@ const val TAG = "BindingAdapter"
 
 @BindingAdapter("timezone")
 fun timezoneTransform(view: TextView, timezone: Int) {
-//    Log.d(TAG, "TimezoneTransform: $timezone")
-    view.text = calTimezone(timezone).toString ()
-//    Log.d(TAG, "TimezoneTransform: ${view.text}")
+    //    Log.d(TAG, "TimezoneTransform: $timezone")
+    val timezone = calTimezone(timezone)
+    when {
+        timezone > 0 -> {
+            view.text = "(GMT +$timezone)"
+        }
+        timezone == 0 -> {
+            view.text = "(GMT)"
+        }
+        else -> {
+            view.text = "(GMT $timezone)"
+        }
+    }
+    //    Log.d(TAG, "TimezoneTransform: ${view.text}")
 }
 
 fun calTimezone(timezone: Int): Int {
-    return (timezone / (60 * 60))
+    return timezone.div(60).div(60)
+}
+
+@BindingAdapter("celsius")
+fun transformCelsius(view: TextView, celsius: Double) {
+    view.text = "${calCelsius(celsius)}°C"
+}
+
+fun calCelsius(celsius: Double): Float {
+    return celsius.div(10).toFloat()
 }
